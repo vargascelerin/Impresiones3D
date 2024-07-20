0f224242daf5aa4fe7e73362f82d836c6922b0db
vargascelerin <vargascelerin@gmail.com>
2024-07-20 17:10:44 -0600

    pagina 3d nav

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+});
